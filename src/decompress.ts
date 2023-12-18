2501440989c3157b70f0dd0bdb8ecbe2cbdb0792
Sam Thorogood <sam.thorogood@gmail.com>
2023-12-19 10:54:11 +1100

    zstd

--- src/decompress.ts
+++ src/decompress.ts
@@ -1,26 +1,55 @@
 import { CompressionCodec } from '../dep/thrift/parquet-code.js';
+import { streamToBytes } from './helper/bytes.ts';
 import { decompress as snappyDecompress } from './snappy/decompress.js';
+
+type DecompressFn = (raw: Uint8Array, uncompressedSize?: number) => Uint8Array;
+
+let zstdPromise: Promise<DecompressFn> | void;
+
+async function prepareZstd(): Promise<DecompressFn> {
+  const zstd = await import('zstddec');
+  const dec = new zstd.ZSTDDecoder();
+  await dec.init();
+  return (arr, uncompressedSize) => dec.decode(arr, uncompressedSize);
+}
 
 /**
  * Decompress a page of Parquet data encoded in one of {@link CompressionCodec}.
  *
  * If the data is uncompressed, this may return the input value.
  */
-export async function decompress(arr: Uint8Array, codec: CompressionCodec): Promise<Uint8Array> {
+export async function decompress(
+  arr: Uint8Array,
+  codec: CompressionCodec,
+  uncompressedSize: number,
+): Promise<Uint8Array> {
+
   switch (codec) {
     case CompressionCodec.UNCOMPRESSED:
       return arr;
 
+    case CompressionCodec.GZIP: {
+      const s = new DecompressionStream('gzip');
+      s.writable.getWriter().write(arr); // TODO: await?
+      const reader: ReadableStreamDefaultReader<Uint8Array> = s.readable.getReader();
+      return streamToBytes(reader);
+    }
+  
     case CompressionCodec.SNAPPY:
       return snappyDecompress(arr);
 
-    case CompressionCodec.GZIP:
-      // TODO: This needs a conditional import for Node or the browser.
-      // The browser should have an optional way to provide `pako` or similar when there's no
-      // decompression stream code available.
-      throw new Error(`TODO: implement gzip via browser/node support`);
+    case CompressionCodec.ZSTD: {
+      if (zstdPromise === undefined) {
+        zstdPromise = prepareZstd();
+      }
+      const decompress = await zstdPromise;
 
-    default:
-      throw new Error(`Unsuppored compression: ${codec}`);
+      // TODO: sometimes `uncompressedSize` is the wrong size, and zstddec just returns an
+      // empty array - don't pass it through for now
+
+      return decompress(arr);
+    }
   }
+
+  throw new Error(`Unsupported codec: ${codec}`)
 }
